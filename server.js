ce7b6ef7b1b34d6da17afe59e74914282d78658e
marb61a <marb61a@gmail.com>
2018-11-12 20:26:32 +0000

    added initial server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const {ApolloServer, gql} = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "variables.env" });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.error(err))
+
+const typeDefs = gql`
+  type Todo {
+    task: String
+    completed: Boolean
+  }
+  type Query {
+    getTodos: [Todo]
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs
+})
