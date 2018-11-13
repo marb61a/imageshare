1f2ae97931592b4ae13adadd7c8d8e8443f8e120
marb61a <marb61a@gmail.com>
2018-11-13 21:16:25 +0000

    updated server.js and added initial typeDefs.gql fil

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const {ApolloServer, gql} = require("apollo-server");
 const mongoose = require("mongoose");
+
 require("dotenv").config({ path: "variables.env" });
+const User = require("./models/User");
+const Post = require("./models/Post");
 
 mongoose
   .connect(
@@ -8,18 +11,17 @@ mongoose
     {useNewUrlParser: true}
   )
   .then(() => console.log("DB Connected"))
-  .catch(err => console.error(err))
-
-const typeDefs = gql`
-  type Todo {
-    task: String
-    completed: Boolean
-  }
-  type Query {
-    getTodos: [Todo]
-  }
-`;
+  .catch(err => console.error(err));
 
 const server = new ApolloServer({
-  typeDefs
-})
+  typeDefs,
+  context: {
+    User,
+    Post
+  }
+});
+
+server.listen()
+  .then(({url}) => {
+    console.log(`Server listening on ${url}`)
+  })
