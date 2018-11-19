6466485febe0132264dd3acaeb9033e415142f10
marb61a <marb61a@gmail.com>
2018-11-19 17:02:39 +0000

    updating App.vue navbar and sidenav

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <v-app>
+    <!-- Side Navbar -->
+    <v-navigation-drawer
+      app
+      temporary
+      fixed
+      v-model="sideNav"
+    >
+    </v-navigation-drawer>
+
     <!-- Horizontal Navbar -->
     <v-toolbar fixed color="primary" dark>
       <v-toolbar-side-icon>
 
       </v-toolbar-side-icon>
-      <v-toolbar-title>
+      <v-toolbar-title class="hidden-xs-only">
         <router-link to="/" tag="span" style="cursor: pointer">
           Imageshare
         </router-link>
@@ -26,16 +35,27 @@
       <v-spacer></v-spacer>
 
       <!-- Horizontal Navbar Links -->
-      <v-toolbar-items>
-        <v-btn flat v-for="item in horizontalNavItems">
-          Posts
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn 
+          flat 
+          v-for="item in horizontalNavItems" 
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-icon class="hidden-sm-only" left>
+            {{item.icon}}
+          </v-icon>
+          {{item.title}}
         </v-btn>
       </v-toolbar-items>
 
     </v-toolbar>
 
+    <!-- App Content -->
     <main>
-      <router-view/>
+      <v-container class="mt-4">
+        <router-view/>
+      </v-container>
     </main>
   </v-app>
 </template>
@@ -43,10 +63,15 @@
 <script>
   export default {
     name: 'App',
+    data() {
+
+    },
     computed: {
       horizontalNavItems() {
         return [
-          {}
+          {icon: "chat", title: 'posts', link: '/posts'},
+          {icon: 'lock_open', title: 'Sign In', link: '/signin'},
+          {icon: 'create', title: 'Sign Up', link: '/signup'}
         ]
       }
     }
