4f0128234abe17668c5b56f3bbb8c8bc8ea439ad
marb61a <marb61a@gmail.com>
2018-11-22 16:27:14 +0000

    added queries file and updated store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { GET_POSTS } from "./queries";
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    posts: [],
+    loading: false
   },
   mutations: {
-
+    setPosts(state, payload) {
+      state.posts = payload
+    },
+    setLoading: (state, payload) => {
+      state.loading = payload;
+    }
   },
   actions: {
-
+    getPosts: ({commit}) => {
+      
+    }
   }
 })
