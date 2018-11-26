7e3787736ab640e400b275398822e5d1aaee328c
marb61a <marb61a@gmail.com>
2018-11-26 16:49:43 +0000

    updated signin vue auth component form and methods

--- client/src/components/Auth/Signin.vue
+++ client/src/components/Auth/Signin.vue
@@ -12,7 +12,7 @@
       <v-flex xs12 sm6 offset-sm3>
         <v-card color="secondary" dark>
           <v-container>
-            <v-form>
+            <v-form @submit.prevent="handleSigninUser">
               <v-layout row>
                 <v-flex xs12>
                   <v-text-field
@@ -25,6 +25,26 @@
                   </v-text-field>
                 </v-flex>
               </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field 
+                    v-model="password" 
+                    prepend-icon="extension" 
+                    label="Password" 
+                    type="password" 
+                    required
+                  >
+                  </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-btn color="accent" type="submit">Signin</v-btn>
+                  <h3>Don't have an account?
+                    <router-link to="/signup">Signup</router-link>
+                  </h3>
+                </v-flex>
+              </v-layout>
             </v-form>
           </v-container>
         </v-card>
@@ -35,10 +55,20 @@
 
 <script>
 export default {
-  name: "Signin"
+  name: "Signin",
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
+  },
+  methods: {
+    handleSigninUser() {
+      this.$store.dispatch("signinUser", {
+        username: this.username,
+        password: this.password
+      })
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
