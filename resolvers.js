335aab5741ff965664eab8d6252faa9a3cebb0f0
marb61a <marb61a@gmail.com>
2018-11-26 17:12:45 +0000

    added getCurrentUser query and resolver

--- resolvers.js
+++ resolvers.js
@@ -10,6 +10,20 @@ const createToken = (user, secret, expiresIn) => {
 
 module.exports = {
   Query: {
+    getCurrentUser: async(_, args, { User, currentUser }) => {
+      if(!currentUser) {
+        return null
+      }
+
+      const user = await User.findOne({
+        username: currentUser.username
+      })
+        .populate({
+          path: "favorites",
+          model: "Post"
+        })
+      return user
+    },
     getPosts: async(_, args, { Post }) => {
       const posts = await Post.find({})
         .sort({
