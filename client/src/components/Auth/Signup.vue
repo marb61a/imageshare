282630475cd97f825d3fa12919bf8f2e33f34071
marb61a <marb61a@gmail.com>
2018-11-28 12:16:56 +0000

    updated Signup auth component

--- client/src/components/Auth/Signup.vue
+++ client/src/components/Auth/Signup.vue
@@ -1,6 +1,36 @@
 <template>
-  <v-container>
-    <h1>Signup</h1>
+  <v-container text-xs-center mt-5 pt-5>
+    <!-- Signup Title -->
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1>Get Started Here</h1>
+      </v-flex>
+    </v-layout>
+
+    <!-- Error Alert -->
+    <v-layout v-if="error" row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <form-alert :message="error.message"></form-alert>
+      </v-flex>
+    </v-layout>
+
+    <!-- Signup Form -->
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card color="accent" dark>
+          <v-container>
+            <v-form
+              v-model="isFormValid" 
+              lazy-validation 
+              ref="form" 
+              @submit.prevent="handleSignupUser"
+            >
+
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
