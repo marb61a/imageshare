34bcafdaba5c3e1243f44f6e153fdefaaa7ce5dc
marb61a <marb61a@gmail.com>
2018-11-29 16:23:44 +0000

    updated store

--- client/src/store.js
+++ client/src/store.js
@@ -1,25 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import router from './router'
 import { defaultClient as apolloClient } from "./main"
-import { GET_POSTS, SIGNIN_USER } from "./queries"
+import { 
+  GET_CURRENT_USER,
+  GET_POSTS, 
+  SIGNIN_USER ,
+  SIGNUP_USER
+} from "./queries"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     posts: [],
-    loading: false
+    user: null,
+    loading: false,
+    error: null,
+    authError: null
   },
   mutations: {
     setPosts(state, payload) {
       state.posts = payload
     },
+    setUser(state, payload) {
+      state.user = payload
+    },
     setLoading: (state, payload) => {
       state.loading = payload;
-    }
+    },
+    setError: (state, payload) => {
+      state.error = payload;
+    },
+    setAuthError: (state, payload) => {
+      state.authError = payload;
+    },
+    clearUser: state => (state.user = null),
+    clearError: state => (state.error = null)
   },
   actions: {
+    getCurrentUser: ({commit}) => {
+      commit("setLoading", true)
+      apolloClient
+        .query({
+          query: GET_CURRENT_USER
+        })
+        .then(({data}) => {
+          
+        })
+    },
     getPosts: ({commit}) => {
       commit("setLoading", true)
       apolloClient
@@ -52,6 +82,9 @@ export default new Vuex.Store({
   },
   getters: {
     posts: state => state.posts,
-    loading: state => state.loading
+    user: state => state.user,
+    loading: state => state.loading,
+    error: state => state.error,
+    authError: state => state.authError
   }
 })
