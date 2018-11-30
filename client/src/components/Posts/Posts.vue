3d9f067adb10bf361fc13f45782af615081a8135
marb61a <marb61a@gmail.com>
2018-11-30 16:54:32 +0000

    Updated Posts.vue component

--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -1,12 +1,46 @@
 <template>
-  <v-container>
-    <h1>Posts</h1>
+  <v-container fluid grid-list-xl>
+    <!-- Post Cards -->
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <v-card-hover>
+          <v-card-media :src="post.imageUrl" height="30vh" lazy>
+          </v-card-media>
+          <v-card-actions>
+            <v-card-title primary>
+              <div>
+                <div class="headline">{{post.title}}</div>
+                <span class="grey--text">
+                  {{post.likes}} likes - {{post.messages.length}} comments
+                </span>
+              </div>
+            </v-card-title>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card-hover>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
+import { INFINITE_SCROLL_POSTS } from "../../queries"
+
+const pageSize = 2
+
 export default {
-  name: "Posts"
+  name: "Posts",
+  data() {
+    return {
+
+    }
+  },
+  apollo: {
+
+  },
+  methods: {
+    
+  }
 }
 </script>
 
