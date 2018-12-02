bdc5d840f9bdae37407f9fe45601adcfe8a3d153
marb61a <marb61a@gmail.com>
2018-12-02 20:24:20 +0000

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill"
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
@@ -7,11 +8,35 @@ import store from './store'
 import ApolloClient from 'apollo-boost'
 import VueApollo, { ApolloProvider } from 'vue-apollo'
 
+import FormAlert from "./components/Shared/FormAlert"
+
+// Register global component
+Vue.component("form-alert", FormAlert) 
+
 Vue.use(VueApollo)
 
 // Setup ApolloClient
 const defaultClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+  uri: 'http://localhost:4000/graphql',
+
+  // Include auth token with requests made to backend
+  fetchOptions: {
+    credentials: "include"
+  },
+  request: operation => {
+    // If there is no token with key of 'token' in localStorage, add it
+    if (!localStorage.token) {
+      localStorage.setItem("token", "");
+    }
+
+    /* Operation adds the token to an authorization header, 
+       which is sent to backend */
+    operation.setContext({
+      headers: {
+        authorization: localStorage.getItem("token")
+      }
+    });
+  }
 })
 
 const apolloProvider = new VueApollo({defaultClient})
