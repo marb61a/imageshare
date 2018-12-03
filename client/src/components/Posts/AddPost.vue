21cc8ceccadc9f04c857d633f9f9c9de0abb6e2f
marb61a <marb61a@gmail.com>
2018-12-03 12:05:53 +0000

    Added intial AddPost form and methods

--- client/src/components/Posts/AddPost.vue
+++ client/src/components/Posts/AddPost.vue
@@ -1,15 +1,45 @@
 <template>
-  <v-container>
-    <h1>Add Post</h1>
+  <v-container text-xs-center mt-5 pt-5>
+    <!-- Add post title -->
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="primary--text">Add Post</h1>
+      </v-flex>
+    </v-layout>
+
+    <!-- Add post form -->
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-form>
+          
+        </v-form>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from "vuex"
+
 export default {
-  name: "Add Post"
+  name: "Add Post",
+  data() {
+    return {
+      isFormValid: true,
+      title: "",
+      imageUrl: "",
+      categories: [],
+      description: ""
+    }
+  },
+  computed: {
+    ...mapGetters(["loading", "user"])
+  },
+  methods: {
+    handleAddPost() {
+
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
