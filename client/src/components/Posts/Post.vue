fd0721202031b52e27cb934b768d3ff9885cab9d
marb61a <marb61a@gmail.com>
2018-12-05 12:03:09 +0000

    Updated Post.vue

--- client/src/components/Posts/Post.vue
+++ client/src/components/Posts/Post.vue
@@ -4,9 +4,41 @@
     <v-layout row wrap>
       <v-flex xs12>
         <v-card hover>
-          <v-card title>
+          <v-card-title>
+            <h1>{{getPost.title}}</h1>
+            <v-btn @click="handleToggleLike" large icon v-if="user">
+              <v-icon 
+                large :color="checkIfPostLiked(getPost._id) ? 'red' : 'grey'"
+              >
+                favorite
+              </v-icon>
+            </v-btn>
+            <h3 class="ml-3 font-weight-thin">
+              {{getPost.likes}} LIKES
+            </h3>
+            <v-spacer></v-spacer>
+            <v-icon @click="goToPreviousPage" color="info" large>
+              arrow_back
+            </v-icon>
+          </v-card-title>
+          <v-tooltip right>
+            <span>Click to enlarge image</span>
+            <v-card-media 
+              @click="toggleImageDialog" 
+              slot="activator" 
+              :src="getPost.imageUrl" 
+              id="post__image"
+            >
+            </v-card-media>
+          </v-tooltip>
 
-          </v-card>
+          <!-- Post image dialog -->
+          <v-dialog v-model="dialog">
+            <v-card>
+              <v-card-media :src="getPost.imageUrl" height="80vh">
+              </v-card-media>
+            </v-card>
+          </v-dialog>
         </v-card>
       </v-flex>
     </v-layout>
