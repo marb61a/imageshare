fbefd062cd0b97628088918057613845bf411d0a
marb61a <marb61a@gmail.com>
2018-12-07 17:15:10 +0000

    apdated typeDefs and User model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -30,5 +32,20 @@ const UserSchema = new mongoose.Schema({
     ref: 'Post'
   }
 });
+
+// Create and add an avatar to a user
+UserSchema.pre("save", function(next) {
+  this.avatar = `http://gravatar.com/avatar/${md5(this.username)}?d=identicon`
+  next()
+})
+
+// Hash the password so that it cannot be seen w/ database access
+UserSchema.pre("save", function(next) {
+  if(!this.isModified("password")) {
+    return next()
+  }
+
+  bcrypt.genSalt
+})
 
 module.exports = mongoose.model('User', UserSchema);
